5ed9c7f84a64da7eacc778010849b6db8a15f09a
SamuelCarneiroSilveira <samuelsilveira14@gmail.com>
2022-02-22 22:09:13 -0300

    hello_world

--- Programa.java
+++ Programa.java
@@ -1,7 +1,7 @@
 // Hello World - java
 
-class Programa {
+public class Programa {
   public static void main(String[] args) {
-    System.out.println("Olá mundo");
+    System.out.println("Hello World!");
   }
 }
