be31c4ca0d1a4e37a45bee00636802dcbecdc594
SamuelCarneiroSilveira <SamuelCarneiroSilveira@gmail.com>
2022-03-09 23:19:25 -0300

    tryCatch

--- 04_Java Exceções: Aprenda a criar, lançar e controlar exceções/java-pilha/src/FluxoComTratramento.java
+++ 04_Java Exceções: Aprenda a criar, lançar e controlar exceções/java-pilha/src/FluxoComTratramento.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComTratramento {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -9,7 +9,7 @@ public class Fluxo {
         	System.out.println("Exception: "+ msg);
         	ex.printStackTrace();
         }
-    
+        
         System.out.println("Fim do main");
     }
 
@@ -21,10 +21,12 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        
-        throw new ArithmeticException("deu errado");        
-        
-        
-       //  System.out.println("Fim do metodo2");        
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            int a = i/0;
+//            Conta c = null;
+//            c.deposita();
+        }
+        System.out.println("Fim do metodo2");        
     }
 }
