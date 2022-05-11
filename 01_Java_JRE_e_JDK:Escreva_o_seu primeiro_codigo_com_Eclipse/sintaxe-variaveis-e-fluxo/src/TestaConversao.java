a41ff198af711c2bbb0abb75a8b50b57a3fc8f54
SamuelCarneiroSilveira <SamuelCarneiroSilveira@gmail.com>
2022-05-11 09:11:51 -0300

    VariáveisEFluxo

--- 01_Java_JRE_e_JDK:Escreva_o_seu primeiro_codigo_com_Eclipse/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
+++
@@ -1,8 +0,0 @@
-
-public class TestaConversao {
-
-	public static void main(String[] args) {
-		int idade = 30;
-		System.out.println("A idade de Marcos é " + idade + "!");
-	}
-}
--- 03_ Java_Polimorfismo:Entenda_herança_e_interfaces/bytebank-herdado-conta/src/Tributavel.java
+++
@@ -1,9 +0,0 @@
-
-// por que interface e n contract 
-
-// tudo dentro de uma interface é abstrato, logo não é necessário escrever abstract
-public abstract interface Tributavel {  // Acho que por contrato
-	
-	double getValorImposto();  // Não preciso escrever public abstract por que é o default
-	
-}
