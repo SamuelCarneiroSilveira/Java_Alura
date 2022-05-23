43c622feda0ff67e881a27ab116e20b80dde7004
SamuelCarneiroSilveira <SamuelCarneiroSilveira@gmail.com>
2022-05-23 18:01:48 -0300

    Instanciação

--- 02_Java_Orientacão_a_Objetos/ByteBank/src/CriaConta.java
+++ 02_Java_Orientacão_a_Objetos/ByteBank/src/CriaConta.java
@@ -3,19 +3,7 @@ public class CriaConta {
 
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
-		primeiraConta.saldo = 200;
-		primeiraConta.saldo += 100;
-		
-		System.out.println("A primeira conta tem: R$" + primeiraConta.saldo);
-		
-		Conta segundaConta = new Conta();
-		segundaConta.saldo = 100;
-		segundaConta.saldo += 150;
-		
-		System.out.println("A segunda conta tem: R$" + segundaConta.saldo);
-		
-		System.out.println(primeiraConta.agencia);
-		System.out.println(primeiraConta.numero);
-		System.out.println(primeiraConta.titular);
-	}
+		primeiraConta.saldo = 100; 
+		System.out.println("O saldo da conta é: " + primeiraConta.saldo);
+	}	
 }
